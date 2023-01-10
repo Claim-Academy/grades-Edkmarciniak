777239afcdc350aeaae5f4a4d1d5f50b84b180ff
Ed Marciniak <ekmarciniak@gmail.com>
2023-01-10 06:11:59 -0600

    WIP Add function to call all grades data

--- app/index.js
+++ app/index.js
@@ -1,1 +1,16 @@
 export const add2Nums = (num1, num2) => num1 + num2;
+
+import apiService from "./api.service.js";
+import {
+  getAllStudentNames,
+  getStudentById,
+  getStudentGradeById,
+} from "./lib.js";
+
+const grades = apiService.getGrades() => {};
+
+const gradesAndNames = getAllStudentNames(
+  getStudentById, getStudentGradeById(2, 3)
+);
+
+
