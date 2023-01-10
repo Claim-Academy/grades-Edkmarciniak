ddfb3b899180047f36b56da98e48c07d8926b3d0
Ed Marciniak <ekmarciniak@gmail.com>
2023-01-10 06:21:01 -0600

    WIP Add functions to lib.js

---
+++ app/lib.js
@@ -0,0 +1,5 @@
+export const getAllStudentNames = (grades) => grades.map((grade) => grade);
+
+export const getStudentById = (grades) => grades.map((grade) => grade);
+
+export const getStudentGradeById = (grades) => grades.map((grade) => grade);
