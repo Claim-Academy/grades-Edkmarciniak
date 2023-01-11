f7af3baaf0efa7a108648e188172d6c1f66727c6
Ed Marciniak <ekmarciniak@gmail.com>
2023-01-11 11:24:20 -0600

    Cleared api.service.js

--- app/api.service.js
+++
@@ -1,6 +0,0 @@
-function getGrades(name, grades) {
-  return name.filter((grades) => grades.grades === grades);
-}
-
-const grades = name.filter((grades) => grades.grades === grades);
-console.log(grades);
