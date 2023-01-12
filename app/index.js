888132b09b088ce8a020854d0600dfcb11505be5
Ed Marciniak <ekmarciniak@gmail.com>
2023-01-12 08:54:17 -0600

    Import function getAllNamesFromList from lib.js

--- app/index.js
+++ app/index.js
@@ -1,5 +1,8 @@
 import apiService from "./api.service.js";
+import { getAllNamesFromList } from "./lib.js";
 
 const gradesInfo = await apiService.getGradesData();
 
 console.log(gradesInfo);
+
+console.log(getAllNamesFromList(gradesInfo));
