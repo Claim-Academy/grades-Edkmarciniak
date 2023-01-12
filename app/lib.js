caaf4805205c3f4f3a31c25d46ce43516538b385
Ed Marciniak <ekmarciniak@gmail.com>
2023-01-11 20:15:51 -0600

    Clear lib directory

--- app/lib.js
+++ app/lib.js
@@ -1,6 +1,1 @@
-export const getTheNamesFromList = (list) => {
-  return list.map((person) => person.name);
-};
 
-export const getAllStudentNames = (grades) => grades.map((grade) => grade);
-return grades.name.map((grade) => grades.name);
