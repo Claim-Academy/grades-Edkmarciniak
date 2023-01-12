21ba7547bdf5de4b432a078203cff0e4e4be6f2f
Ed Marciniak <ekmarciniak@gmail.com>
2023-01-11 22:09:24 -0600

    Unit test passed for getStudentById

--- app/lib.test.js
+++ app/lib.test.js
@@ -1,9 +1,22 @@
 import { expect, it } from "vitest";
-import { getAllNamesFromList } from "./lib.js";
+import { getAllNamesFromList, getStudentById } from "./lib.js";
 
 it("should return all names from list", () => {
   const input = [{ name: "John" }, { name: "Jane" }];
   const expected = ["John", "Jane"];
+
   const actual = getAllNamesFromList(input);
+
+  expect(actual).toEqual(expected);
+});
+
+it("should return student by id", () => {
+  const listInput = [{ id: 1 }, { id: 2 }];
+  const idFindInput = 2;
+
+  const expected = { id: 2 };
+
+  const actual = getStudentById(listInput, idFindInput);
+
   expect(actual).toEqual(expected);
 });
