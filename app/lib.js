c8c9a4b1367286febb0b6bcc9bb3a54b1e98a18b
Ed Marciniak <ekmarciniak@gmail.com>
2023-01-11 22:15:02 -0600

    Add function for getStudentGradeById

--- app/lib.js
+++ app/lib.js
@@ -2,3 +2,6 @@ export const getAllNamesFromList = (list) => list.map((people) => people.name);
 
 export const getStudentById = (list, id) =>
   list.find((employee) => employee.id === id);
+
+export const getStudentGradeById = (list, idGrade) =>
+  list.find((employee) => employee.id === idGrade);
